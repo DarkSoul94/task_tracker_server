27be2629aab274a94c652c35b536cef11a81d13f
IrDeTen <kellxus1@gmail.com>
2021-08-02 16:57:26 +0300

    feature: add middleware to prase authorization token

--- auth/usecase.go
+++ auth/usecase.go
@@ -1,10 +1,15 @@
 package auth
 
-import "github.com/DarkSoul94/task_tracker_server/models"
+import (
+	"context"
+
+	"github.com/DarkSoul94/task_tracker_server/models"
+)
 
 // Usecase ...
 type AuthUC interface {
 	SignIn(inpUser *models.LoginUser) (models.User, error)
 	SignUp(user *models.LoginUser) (models.User, error)
 	GenerateToken(user *models.User) (string, error)
+	ParseToken(ctx context.Context, accessToken string) (*models.User, error)
 }
